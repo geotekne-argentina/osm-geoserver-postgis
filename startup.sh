3628e37a73f0e95cfd33f3dba214fb4c143acfff
Jose Macchi <jose.macchi@gmail.com>
2022-05-19 00:26:23 -0300

    Remove files. Fix resource download link

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ FILE=./data/geoserver/data_dir/data/osm-lowres.gpkg
 if [ ! -f "$FILE" ]; then
     echo -e "\n ----- OSM Low resolution file ($FILE) does not exist."
     echo -e "\n ----- Download and configure file in data folder."
-    curl "https://link.us1.storjshare.io/s/jxjnkchpopbfqozdoz5r3x56mebq/osm/osm-lowres-modified.zip?download=1" -o ./osm-lowres-modified.zip
+    curl "https://link.us1.storjshare.io/s/jumximeaymxkett2lt66cid2l4pq/osm/osm-lowres-modified.zip?download=1" -o ./osm-lowres-modified.zip
     unzip -o ./osm-lowres-modified.zip
     mv ./osm-lowres-modified.gpkg ./data/geoserver/data_dir/data/osm-lowres.gpkg
 fi
