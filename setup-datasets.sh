dde133d86087245845eb0f16586a23602865debb
Jose Macchi <jose.macchi@gmail.com>
2022-11-10 17:52:22 -0300

    Update on setup-datasets process

--- setup-datasets.sh
+++ setup-datasets.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 echo -e "\n Setup High and Low resolution datasets."
 
-FILE=./data/geoserver/data_dir/data/osm-lowres.gpkg
+DATAFOLDER=./data/geoserver/data_dir/data
+FILE=$DATAFOLDER/osm-lowres.gpkg
 if [ ! -f "$FILE" ]; then
     echo -e "\n ----- OSM Low resolution file ($FILE) does not exist."
     echo -e "\n ----- Download and configure file in data folder."
-    curl "https://link.us1.storjshare.io/s/jumximeaymxkett2lt66cid2l4pq/osm/osm-lowres-modified.zip?download=1" -o ./osm-lowres-modified.zip
+    curl -k "https://link.us1.storjshare.io/s/jumximeaymxkett2lt66cid2l4pq/osm/osm-lowres-modified.zip?download=1" -o ./osm-lowres-modified.zip
     unzip -o ./osm-lowres-modified.zip
-    mv ./osm-lowres-modified.gpkg ./data/geoserver/data_dir/data/osm-lowres.gpkg
+    rm ./osm-lowres-modified.zip
+    mv ./osm-lowres-modified.gpkg $FILE
+    mv ./README.txt $DATAFOLDER/README.txt
 fi
 
 # Download my Selection PBF (ie. Suriname)
@@ -15,5 +18,5 @@ FILE=./pbfs/suriname-latest.osm.pbf
 if [ ! -f "$FILE" ]; then
     echo -e "\n ----- OSM High resolution file ($FILE) does not exist."
     echo -e "\n ----- Download and configure file in data folder."
-    curl "https://download.geofabrik.de/south-america/suriname-latest.osm.pbf" -o ./pbfs/suriname-latest.osm.pbf
+    curl -k "https://download.geofabrik.de/south-america/suriname-latest.osm.pbf" -o $FILE
 fi
