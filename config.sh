12e4c48c814b154f6813e833e477ad39cb4502c4
Jose Macchi <jose.macchi@gmail.com>
2024-05-15 15:46:44 -0300

    Changes required to include fonts for Taiwan. Config file pointing to Taiwan source.

--- config.sh
+++ config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #  my selected PBF (ie. Suriname)
-PBF_URL="https://download.geofabrik.de/south-america/suriname-latest.osm.pbf"
+#PBF_URL="https://download.geofabrik.de/south-america/suriname-latest.osm.pbf"
+PBF_URL="https://download.geofabrik.de/asia/taiwan-latest.osm.pbf"
